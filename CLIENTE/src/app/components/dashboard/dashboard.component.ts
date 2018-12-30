d96d843c8593a5b74a2008f60a954db2b0b76bdd
daniel hidalgo <hrdi3191@gmail.com>
2018-12-30 08:57:44 -0500

    agrego modal

--- CLIENTE/src/app/components/dashboard/dashboard.component.ts
+++ CLIENTE/src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare const M;
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -8,18 +8,37 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
   constructor() {
+    //inicializacion de menu
     document.addEventListener('DOMContentLoaded', function() {
       var elems = document.querySelectorAll('.sidenav');
       var instances = M.Sidenav.init(elems, {
         edge: 'left',
         draggable: true,
+        inDuration: null,
+        outDuration: 200,
+        onOpenStart: null,
+        onOpenEnd: null,
+        onCloseStart: null,
+        onCloseEnd: null,
+        preventScrolling: true,
+        dismissible:false
+      });
+    });
+
+    //inicializacion de modal1
+
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.modal');
+      var instances = M.Modal.init(elems, {
+
+
         inDuration: 250,
         outDuration: 200,
         onOpenStart: null,
         onOpenEnd: null,
         onCloseStart: null,
         onCloseEnd: null,
-        preventScrolling: true
+        preventScrolling: false
       });
     });
 
@@ -29,5 +48,7 @@ export class DashboardComponent implements OnInit {
 
 
   }
+
+
 
 }
